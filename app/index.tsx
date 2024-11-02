397af269035772e862f2318de561423b0a7d8d28
Kavosh Madadi <kavoshmadadiglk@gmail.com>
2024-11-02 22:33:33 +0330

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function MainPage() {
+  return (
+    <View>
+      <Text>MainPage</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
